21abb68efae1a0aad1f4082336afba054d6e6ecc
Minh Vo <minhvo4398@gmail.com>
2024-05-19 11:08:06 +0700

    update

--- tests/poke-api.spec.js
+++ tests/poke-api.spec.js
@@ -4,4 +4,5 @@ test("Pokemon information", async ({ request }) => {
   const pokemon = await request.get("pokemon/squirtle");
   let pokeJson = await pokemon.json();
   console.log(await pokeJson.moves[12].move.name);
+  console.log("Test");
 });
